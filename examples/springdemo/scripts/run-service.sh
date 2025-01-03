1c0ddefe081cf70f3b453c16dcd3e5081aef7471
Islam M <mahgisla@amazon.co.uk>
2025-01-03 23:13:19 +0000

    feat(warp cr): change crac cr engine for springdemo to warp

--- examples/springdemo/scripts/run-service.sh
+++ examples/springdemo/scripts/run-service.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # run java
 echo Starting the application...
-java -Dspring.profiles.active=prod -Dtable.name=${TABLE_NAME} -Dmode=${MODE} -Damazon.dynamodb.endpoint=${AMAZON_DYNAMO_DB_ENDPOINT} -XX:CRaCCheckpointTo=/opt/crac-files -jar /${SRVC_JAR_FILE_NAME}
+java -Dspring.profiles.active=prod -Dtable.name=${TABLE_NAME} -Dmode=${MODE} -Damazon.dynamodb.endpoint=${AMAZON_DYNAMO_DB_ENDPOINT} -jar /${SRVC_JAR_FILE_NAME}
